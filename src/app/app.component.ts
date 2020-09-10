d0883e596f2a60274a4a260c9f5477ef1e0ee520
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-10 19:07:07 +0530

    Angular complete form validation completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,18 @@ export class AppComponent {
     '',
     'bharathjinka09@gmail.com',
     9110334114,
-    '',
+    'default',
     'morning',
     true
   );
+
+  topicHasError = true;
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
