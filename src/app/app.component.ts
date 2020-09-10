195f40a596385bc3eb5654119ac321ffc788f94c
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-10 17:04:29 +0530

    Angular form ngModel, ngForm, ngModelGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User(
+    'Bharath',
+    'bharathjinka09@gmail.com',
+    9110334114,
+    '',
+    'morning',
+    true
+  );
 }
